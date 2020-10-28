c84e3c927c564f083fe2eb0d6b84072716ddd7fa
Ilya <kozyrev.frontend@gmail.com>
2020-10-28 15:11:55 +0300

    выносит данные каталога в отдельный файл

--- source/js/components/state.js
+++ source/js/components/state.js
@@ -1,250 +1,8 @@
+import { catalog } from './catalog';
+
 class State {
-  constructor() {
-    this.catalogData = {
-      SO757575: {
-        articule: `SO757575`,
-        model: `Честер Bass`,
-        type: `электрогитара`,
-        popularity: 15,
-        stringsNumber: 7,
-        price: `17500`,
-        image: `card-electro1`
-      },
-      TK129049: {
-        articule: `TK129049`,
-        model: `СURT Z300`,
-        type: `электрогитара`,
-        popularity: 9,
-        stringsNumber: 7,
-        price: `29500`,
-        image: `card-electro2`
-      },
-      RO111111: {
-        articule: `RO111111`,
-        model: `Roman LX`,
-        type: `укулеле`,
-        popularity: 21,
-        stringsNumber: 4,
-        price: `6800`,
-        image: `card-ukulele1`
-      },
-      TK436457: {
-        articule: `TK436457`,
-        model: `СURT T300`,
-        type: `электрогитара`,
-        popularity: 15,
-        stringsNumber: 6,
-        price: `30000`,
-        image: `card-electro3`
-      },
-      DI192138: {
-        articule: `DI192138`,
-        model: `Dania Super`,
-        type: `акустическая гитара`,
-        popularity: 5,
-        stringsNumber: 7,
-        price: `3500`,
-        image: `card-ukulele2`
-      },
-      SO934345: {
-        articule: `SO934345`,
-        model: `Честер WX`,
-        type: `электрогитара`,
-        popularity: 17,
-        stringsNumber: 6,
-        price: `15300`,
-        image: `card-electro1`
-      },
-      DI082347: {
-        articule: `DI082347`,
-        model: `Dania VX`,
-        type: `укулеле`,
-        popularity: 5,
-        stringsNumber: 4,
-        price: `2200`,
-        image: `card-ukulele1`
-      },
-      SO135646: {
-        articule: `SO135646`,
-        model: `Честер Plus`,
-        type: `электрогитара`,
-        popularity: 27,
-        stringsNumber: 4,
-        price: `30000`,
-        image: `card-electro4`
-      },
-      VO154751: {
-        articule: `VO154751`,
-        model: `Виолана 300`,
-        type: `акустическая гитара`,
-        popularity: 3,
-        stringsNumber: 7,
-        price: `1700`,
-        image: `card-ukulele2`
-      },
-      TK244556: {
-        articule: `TK244556`,
-        model: `СURT Clasic`,
-        type: `электрогитара`,
-        popularity: 20,
-        stringsNumber: 4,
-        price: `23000`,
-        image: `card-electro2`
-      },
-      TK134663: {
-        articule: `TK134663`,
-        model: `СURT Z250`,
-        type: `электрогитара`,
-        popularity: 19,
-        stringsNumber: 4,
-        price: `18700`,
-        image: `card-electro1`
-      },
-      SO123212: {
-        articule: `SO123212`,
-        model: `Честер 7X`,
-        type: `электрогитара`,
-        popularity: 30,
-        stringsNumber: 7,
-        price: `35000`,
-        image: `card-electro3`
-      },
-      SO123234: {
-        articule: `SO123234`,
-        model: `Честер 6V`,
-        type: `электрогитара`,
-        popularity: 28,
-        stringsNumber: 6,
-        price: `14900`,
-        image: `card-electro4`
-      },
-      VO519510: {
-        articule: `VO519510`,
-        model: `Виолана Mix`,
-        type: `акустическая гитара`,
-        popularity: 7,
-        stringsNumber: 6,
-        price: `7600`,
-        image: `card-ukulele2`
-      },
-      VO457369: {
-        articule: `VO457369`,
-        model: `Виолана 250x`,
-        type: `акустическая гитара`,
-        popularity: 19,
-        stringsNumber: 6,
-        price: `6500`,
-        image: `card-ukulele2`
-      },
-      FB625903: {
-        articule: `FB625903`,
-        model: `Фабио Лайт`,
-        type: `акустическая гитара`,
-        popularity: 26,
-        stringsNumber: 7,
-        price: `12000`,
-        image: `card-ukulele2`
-      },
-      FB576948: {
-        articule: `FB576948`,
-        model: `Фабио L100`,
-        type: `акустическая гитара`,
-        popularity: 31,
-        stringsNumber: 7,
-        price: `9900`,
-        image: `card-ukulele2`
-      },
-      LU012032: {
-        articule: `LU012032`,
-        model: `Liana Z200`,
-        type: `акустическая гитара`,
-        popularity: 28,
-        stringsNumber: 12,
-        price: `8900`,
-        image: `card-ukulele2`
-      },
-      LU546853: {
-        articule: `LU546853`,
-        model: `Liana Z100`,
-        type: `акустическая гитара`,
-        popularity: 34,
-        stringsNumber: 12,
-        price: `10500`,
-        image: `card-ukulele2`
-      },
-      LU458283: {
-        articule: `LU458283`,
-        model: `Liana Z300`,
-        type: `акустическая гитара`,
-        popularity: 9,
-        stringsNumber: 6,
-        price: `13300`,
-        image: `card-ukulele2`
-      },
-      RO324341: {
-        articule: `RO324341`,
-        model: `Roman RX`,
-        type: `укулеле`,
-        popularity: 37,
-        stringsNumber: 4,
-        price: `4800`,
-        image: `card-ukulele1`
-      },
-      RO214235: {
-        articule: `RO214235`,
-        model: `Roman TX`,
-        type: `укулеле`,
-        popularity: 5,
-        stringsNumber: 4,
-        price: `1900`,
-        image: `card-ukulele1`
-      },
-      DI132414: {
-        articule: `DI132414`,
-        model: `Dania U100`,
-        type: `укулеле`,
-        popularity: 23,
-        stringsNumber: 4,
-        price: `2500`,
-        image: `card-ukulele1`
-      },
-      DI934754: {
-        articule: `DI934754`,
-        model: `Dania WR`,
-        type: `укулеле`,
-        popularity: 3,
-        stringsNumber: 4,
-        price: `3800`,
-        image: `card-ukulele1`
-      },
-      DI034292: {
-        articule: `DI034292`,
-        model: `Dania LE`,
-        type: `укулеле`,
-        popularity: 10,
-        stringsNumber: 4,
-        price: `4100`,
-        image: `card-ukulele1`
-      },
-      MI193214: {
-        articule: `MI193214`,
-        model: `Mirana V10`,
-        type: `укулеле`,
-        popularity: 14,
-        stringsNumber: 4,
-        price: `2700`,
-        image: `card-ukulele1`
-      },
-      VO043244: {
-        articule: `VO043244`,
-        model: `Виолана Mini`,
-        type: `укулеле`,
-        popularity: 29,
-        stringsNumber: 4,
-        price: `6700`,
-        image: `card-ukulele1`
-      }
-    };
+  constructor(data) {
+    this.catalogData = data;
 
     this.filters = {
       price: {
@@ -265,104 +23,104 @@ class State {
     this.catalogItemsPerPage = 9;
 
     this.itemsOffset = 0;
+  }
 
-    this.clearFilters = () => {
-      this.filters.type.splice(0, this.filters.type.length);
-      this.filters.strings.splice(0, this.filters.strings.length);
-    };
+  clearFilters() {
+    this.filters.type.splice(0, this.filters.type.length);
+    this.filters.strings.splice(0, this.filters.strings.length);
+  }
 
-    this.getTotalPagesCount = (data) => {
-      const catalogItemsCount = Object.values(data).length;
+  getTotalPagesCount(dataObject) {
+    const catalogItemsCount = Object.values(dataObject).length;
 
-      return Math.ceil(catalogItemsCount / this.catalogItemsPerPage);
-    };
+    return Math.ceil(catalogItemsCount / this.catalogItemsPerPage);
+  }
 
-    this.getCatalogDataPerPage = (data) => {
-      return Object.values(data).slice(this.itemsOffset, this.itemsOffset + this.catalogItemsPerPage);
-    };
+  getCatalogDataPerPage(dataObject) {
+    return Object.values(dataObject).slice(this.itemsOffset, this.itemsOffset + this.catalogItemsPerPage);
+  }
 
-    this.setCurrentPage = (value) => {
-      this.currentPage = value;
-    };
+  setCurrentPage(value) {
+    this.currentPage = value;
+  }
 
-    this.setItemsOffset = () => {
-      this.itemsOffset = (this.currentPage - 1) * this.catalogItemsPerPage;
-    };
+  setItemsOffset() {
+    this.itemsOffset = (this.currentPage - 1) * this.catalogItemsPerPage;
+  }
 
-    this.getCatalogData = () => {
-      return this.catalogData;
-    };
+  getCatalogData() {
+    return this.catalogData;
+  }
 
-    this.setFiltersPriceMin = (value) => {
-      this.filters.price.min = value;
-    };
+  setFiltersPriceMin(value) {
+    this.filters.price.min = value;
+  }
 
-    this.setFiltersPriceMax = (value) => {
-      this.filters.price.max = value;
-    };
+  setFiltersPriceMax(value) {
+    this.filters.price.max = value;
+  }
 
-    this.setFiltersType = (value) => {
-      this.filters.type.push(value);
-    };
+  setFiltersType(value) {
+    this.filters.type.push(value);
+  }
 
-    this.setFiltersStrings = (value) => {
-      this.filters.strings.push(value);
-    };
+  setFiltersStrings(value) {
+    this.filters.strings.push(value);
+  }
 
-    this.getFilteredCatalogData = () => {
-      const currentData = Object.values(this.catalogData);
+  getFilteredCatalogData() {
+    const currentData = Object.values(this.catalogData);
 
-      const filteredData = currentData.filter((item) => {
-        if (parseInt(item.price, 10) < parseInt(this.filters.price.min, 10) || parseInt(item.price, 10) > parseInt(this.filters.price.max, 10)) {
-          return false;
-        }
+    const filteredData = currentData.filter((item) => {
+      if (parseInt(item.price, 10) < parseInt(this.filters.price.min, 10) || parseInt(item.price, 10) > parseInt(this.filters.price.max, 10)) {
+        return false;
+      }
 
-        if (!this.filters.type.includes(item.type)) {
-          return false;
-        }
+      if (!this.filters.type.includes(item.type)) {
+        return false;
+      }
 
-        if (!this.filters.strings.includes(item.stringsNumber)) {
-          return false;
-        }
+      if (!this.filters.strings.includes(item.stringsNumber)) {
+        return false;
+      }
 
-        return true;
-      });
+      return true;
+    });
 
-      return filteredData;
-    };
+    return filteredData;
+  }
 
-    this.setSortType = (value) => {
-      this.sort.type = value;
-    };
+  setSortType(value) {
+    this.sort.type = value;
+  }
 
-    this.setSortFlow = (value) => {
-      this.sort.flow = value;
-    };
+  setSortFlow(value) {
+    this.sort.flow = value;
+  }
 
-    this.getDataSortedByPrice = (data) => {
-      const catalogData = Object.values(data);
+  getDataSortedByPrice(dataObject) {
+    const catalogData = Object.values(dataObject);
 
-      return catalogData.sort((a, b) => {
-        if (this.sort.flow === `down`) {
-          return parseInt(b.price, 10) - parseInt(a.price, 10);
-        } else {
-          return parseInt(a.price, 10) - parseInt(b.price, 10);
-        }
-      });
-    };
+    return catalogData.sort((a, b) => {
+      if (this.sort.flow === `down`) {
+        return parseInt(b.price, 10) - parseInt(a.price, 10);
+      } else {
+        return parseInt(a.price, 10) - parseInt(b.price, 10);
+      }
+    });
+  }
 
-    this.getDataSortedByPopularity = (data) => {
-      const catalogData = Object.values(data);
+  getDataSortedByPopularity(dataObject) {
+    const catalogData = Object.values(dataObject);
 
-      return catalogData.sort((a, b) => {
-        if (this.sort.flow === `down`) {
-          return b.popularity - a.popularity;
-        } else {
-          return a.popularity - b.popularity;
-        }
-      });
-    };
+    return catalogData.sort((a, b) => {
+      if (this.sort.flow === `down`) {
+        return b.popularity - a.popularity;
+      } else {
+        return a.popularity - b.popularity;
+      }
+    });
   }
 }
 
-export const state = new State();
+export const state = new State(catalog);
